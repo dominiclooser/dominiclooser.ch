6537f04dd58e2ace88ae287e3ecfa432fbc534bc
Dominic Looser <dominic.looser@gmail.com>
2018-12-11 02:35:46 +0100

    Updates

--- scripts/masonry.js
+++ scripts/masonry.js
@@ -11,7 +11,9 @@
     return new Masonry('.masonry', options);
   };
 
-  $('.masonry').imagesLoaded(draw);
+  $('.masonry').imagesLoaded(function() {
+    return setTimeout(draw, 800);
+  });
 
   lightbox.option({
     showImageNumberLabel: false
