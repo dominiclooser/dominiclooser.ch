4909c037c209cd4ace909840dfa305738fe64d2d
Dominic Looser <dominic.looser@gmail.com>
2020-05-03 01:09:35 +0200

    misc

--- dynamic/scripts/wikidata.coffee
+++ dynamic/scripts/wikidata.coffee
@@ -1,4 +1,44 @@
-showOverlay = (id) ->
+showOverlay = (id, lang) ->
+
+    get_props = (entity) ->
+        claims = entity.claims
+        types = claims['P31']
+        if 'Q11424' in types # film
+            props = ['P57', 'P58', 'P495', 'P272']  # dir, screenwriter, country, prod comp
+        return props
+
+    render = (entity) ->
+        props = get_props(entity)
+
+        dl = "<dl class='overlay-props'>"
+        for prop in props
+            value = entity.claims[prop]
+            dl += """
+            <dt><wd-entity id=#{prop} lang=#{lang}></dt>
+            <dd><wd-entity id=#{entity.id} property=#{prop} lang=#{lang}></dd>
+            """
+        dl += '</dl>'
+        links = """
+            <ul>
+                <li><a is='wd-link' entity-id=#{entity.id} site='enwiki'>English Wikipedia</a></li>
+                <li><a is='wd-link' entity-id=#{entity.id} property='P856'>Official Website</a></li>
+                <li><a is='wd-link' entity-id=#{entity.id} property='P345'>IMDB</a></li>
+            </ul>
+        """
+        overlay = document.createElement('div')
+        overlay.classList.add('overlay')
+        overlay.innerHTML = """
+            <div class='overlay-content'>
+                <h4 class='overlay-title'>
+                    <wd-entity id=#{entity.id} label lang=#{lang}>
+                </h4>
+                <div class='overlay-main'>
+                    #{dl}
+                    #{links}
+                </div>
+            </div>
+        """
+        document.body.append(overlay)
 
     wikidata = WBK
         instance: 'https://www.wikidata.org'
@@ -9,21 +49,19 @@ showOverlay = (id) ->
         .then (response) -> 
             response.json()
         .then wikidata.parse.wd.entities
-        .then (entities) -> 
-            console.log entities[id].claims
+        .then (entities) ->
+            entity = entities[id] 
+            
 
-    overlay = document.createElement('div')
-    overlay.classList.add('overlay')
-    overlay.innerHTML = """
-        <div class='overlay'>
-        #{id}
-        </div>
-    """
-    document.body.append(overlay)
+            render(entity)
+
+    
     
 
 buttons = document.querySelectorAll('.show-item')
 for button in buttons
     button.addEventListener 'click', ->
         id = this.dataset.id
-        showOverlay(id)
+        showOverlay(id, 'en')
+
+showOverlay('Q669591', 'en')
--- out/scripts/wikidata.js
+++ out/scripts/wikidata.js
@@ -1,22 +1,51 @@
 (function() {
-  var button, buttons, i, len, showOverlay;
+  var button, buttons, i, len, showOverlay,
+    indexOf = [].indexOf;
 
-  showOverlay = function(id) {
-    var overlay, url, wikidata;
+  showOverlay = function(id, lang) {
+    var get_props, render, url, wikidata;
+    get_props = function(entity) {
+      var claims, props, types;
+      claims = entity.claims;
+      types = claims['P31'];
+      if (indexOf.call(types, 'Q11424') >= 0) { // film
+        props = [
+          'P57',
+          'P58',
+          'P495',
+          'P272' // dir, screenwriter, country, prod comp
+        ];
+      }
+      return props;
+    };
+    render = function(entity) {
+      var dl, i, len, links, overlay, prop, props, value;
+      props = get_props(entity);
+      dl = "<dl class='overlay-props'>";
+      for (i = 0, len = props.length; i < len; i++) {
+        prop = props[i];
+        value = entity.claims[prop];
+        dl += `<dt><wd-entity id=${prop} lang=${lang}></dt>\n<dd><wd-entity id=${entity.id} property=${prop} lang=${lang}></dd>`;
+      }
+      dl += '</dl>';
+      links = `<ul>\n    <li><a is='wd-link' entity-id=${entity.id} site='enwiki'>English Wikipedia</a></li>\n    <li><a is='wd-link' entity-id=${entity.id} property='P856'>Official Website</a></li>\n    <li><a is='wd-link' entity-id=${entity.id} property='P345'>IMDB</a></li>\n</ul>`;
+      overlay = document.createElement('div');
+      overlay.classList.add('overlay');
+      overlay.innerHTML = `<div class='overlay-content'>\n    <h4 class='overlay-title'>\n        <wd-entity id=${entity.id} label lang=${lang}>\n    </h4>\n    <div class='overlay-main'>\n        ${dl}\n        ${links}\n    </div>\n</div>`;
+      return document.body.append(overlay);
+    };
     wikidata = WBK({
       instance: 'https://www.wikidata.org',
       sparqlEndpoint: 'https://query.wikidata.org/sparql'
     });
     url = wikidata.getEntities(id);
-    fetch(url).then(function(response) {
+    return fetch(url).then(function(response) {
       return response.json();
     }).then(wikidata.parse.wd.entities).then(function(entities) {
-      return console.log(entities[id].claims);
+      var entity;
+      entity = entities[id];
+      return render(entity);
     });
-    overlay = document.createElement('div');
-    overlay.classList.add('overlay');
-    overlay.innerHTML = `<div class='overlay'>\n${id}\n</div>`;
-    return document.body.append(overlay);
   };
 
   buttons = document.querySelectorAll('.show-item');
@@ -26,8 +55,10 @@
     button.addEventListener('click', function() {
       var id;
       id = this.dataset.id;
-      return showOverlay(id);
+      return showOverlay(id, 'en');
     });
   }
+
+  showOverlay('Q669591', 'en');
 
 }).call(this);
